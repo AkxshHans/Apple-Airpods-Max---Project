a1eb83c3bc6475ad829aa8f03fd012c5e37c2952
Akash Hans <162800417+AkxshHans@users.noreply.github.com>
2024-04-03 23:52:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+Shery.makeMagnet(" .main , .main2" /* Element to target.*/, {
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
+
+
+  Shery.hoverWithMediaCircle(".main2 img" /* Element to target.*/, {
+    images: ["maxwhite,png", "image2.jpg", "image3.jpg"] /*OR*/,
+    //videos: ["video1.mp4", "video2.mp4"],
+  });
